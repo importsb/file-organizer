2cb494bdff670328f4217ee42d91400300684863
importsb <shayne.bush1@gmail.com>
2025-08-20 17:09:57 -0700

    Update file_organizer_main.py
    removed fancy_printer()
    renamed bank to folder_factory()
    edited and added to notes for each function explicitly explaining variables and actions of function
    changed scanner
    added multiple variables
    imported shutil
    added break to main

--- file_organizer_main.py
+++ file_organizer_main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def is_folder(folder):
     """
@@ -10,50 +11,97 @@ def is_folder(folder):
     else:
         return False
 
-def is_valid_dir(full_path):
+def folder_factory(folder, file_ext):
     """
+    Creates subfolder with ext if it does not exist
+    If it does exist, do nothing
+    declares
+        ext = extension
+        tar_dir = target directory(folder)
+    returns tar_dir
     """
-def bank(file_name, file_ext):
-    """
-    """
+    ext = file_ext.lower().lstrip(".")
+    if ext == "":
+        ext = "other"
+    tar_dir = os.path.join(folder, ext)
+    if os.path.isdir(tar_dir):
+        print(f'Using folder: {tar_dir}')
+    else:
+        os.makedirs(tar_dir)
+        print(f'Created folder: {tar_dir}')
+    return tar_dir
 
-def move(full_path, tar_dir, file_name):
+def move(full_path, tar_dir, name):
     """
+     Moves files into tar_dir, uses incrementor to avoid overwriting
+     declares
+        end = destination folder
+        base = source folder
+        ext = file extension
+        i = incrementor
     """
+    end = os.path.join(tar_dir, name)
+    base, ext = os.path.splitext(name)
+    i = 1
+    while os.path.exists(end):
+        end = os.path.join(tar_dir, f'{base}({i}){ext}') #adds folder
+        i += 1
+    print(f'Moving: {full_path} to {end}')
+    shutil.move(full_path, end)
+    print(f'Moved: {full_path} to {end}')
+
 def scanner(folder):
     """
     Scans the files within the folder specified.
-    Gets name, file_name, file_ext
+    declares
+        total = found number of files
+        moved = moved files
+        skipped = skipped files
+        name = folder
+        full_path = source folder path
+        file_name = file
+        file_ext = extension
+        ext = extension with "." or ""
+        tar_dir = target directory
     prints what is found and loops through the files
+    calls folder_factory
+    calls move
     """
-    for name in os.listdir(folder):
-        full_path = os.path.join(folder, name)
-        if os.path.isfile(full_path):
-            file_name, file_ext = os.path.splitext(name)
-            file_ext = file_ext.lower()
-            print(f"Found file: {name}, extension: {file_ext}")
+    total = 0
+    moved = 0
+    skipped = 0
+    print(f'Scanning: {folder}')
+    for name in os.listdir(folder): #gets list of names inside folder
+        full_path = os.path.join(folder, name) #sets full_path to path of user input folder
+        if os.path.isfile(full_path): #if exists
+            total += 1
+            file_name, file_ext = os.path.splitext(name) #splits name and extension
+            print(f'Found:{name} (ext:{file_ext})')
+            tar_dir = folder_factory(folder, file_ext)
+            move(full_path, tar_dir, name)
+            moved += 1
         else:
-            print(f"Skipping folder: {name}")
-
-def fancy_printer(folder):
-    """
-    Prints in a fancy way.
-    """
-    print(f"Folder: " + folder + str(os.path.splitext(folder)[0]))
-
-
+            print(f'Skipping {name}')
+            skipped += 1
+    print(f'Found:{total}, Moved:{moved}, Skipped:{skipped}')
 
 def main():
     """
     Gets user input
     Calls is_folder
+        Reprompts if folder does not exist
+    Calls scanner
+    declares folder
+    ends once completed
     """
-    while True:
+    while True: #prompts for userinput for folder. ch
         folder = input("What folder do you need organized?(Enter the folder path): ")
         if is_folder(folder):
-            fancy_printer(folder)
+            scanner(folder)
+            print("Completed all operations.")
+            break
         else:
-            print("The folder doesn't exist")
+            print("The folder does not exist")
 
 
 
